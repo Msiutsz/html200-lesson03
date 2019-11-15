422bfd5c45f61ff84b76a4d9d5f517593675e38f
Martha Tran <siutsz@uw.edu>
2019-11-15 15:39:29 -0800

    exercise-one completed

---
+++ exercise-one.js
@@ -0,0 +1,13 @@
+let rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'];
+
+rainbowColors.push('violet');
+
+console.log(rainbowColors);
+
+rainbowColors.splice(2, 1);
+
+console.log(rainbowColors);
+
+const rainbowColorsList = rainbowColors.join (', ');
+
+console.log(rainbowColors.join (', '));
